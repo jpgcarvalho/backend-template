76a02dffb3250d07fa7f99f3ebdba21bcff818f6
João Pedro <peugomescarvalho2@gmail.com>
2023-09-01 21:37:45 -0300

    refatorando

--- src/business/Interfaces/Prisma/IProfile.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from "./IUser";
-
-export interface IProfile {
-  id: number;
-  bio?: string | null;
-  user: IUser;
-  userId: string;
-}
--- src/business/Interfaces/Prisma/IUser.ts
+++ src/business/Interfaces/Prisma/IUser.ts
@@ -1,10 +1,8 @@
 import { IPost } from "./IPost";
-import { IProfile } from "./IProfile";
 
 export interface IUser {
   id: number;
   email: string;
   name?: string | null;
   posts: IPost[];
-  profile?: IProfile | null;
 }
